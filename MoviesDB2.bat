e7587c1b728c4ba6b3ac9e2fca435a961dd504c8
Carl Louie Aruta <arutacarllouie@yahoo.com>
2017-03-02 14:42:51 +0800

    v.2 Added bat file for database setup. Working Login. Added DAO and Domain classes.

---
+++ MoviesDB2.bat
@@ -0,0 +1,6 @@
+db2cmd db2 -tvf C:\Users\clgaruta\Documents\StarDB\MoviesDB2.txt
+@echo off 
+echo ----------------------------
+echo Movies Database Installed
+echo ----------------------------
+exit
---
+++ src/Ticket.java
@@ -0,0 +1,17 @@
+// Class that contains methods for Database manipulation
+
+
+public class Ticket
+{
+	private static String totalS;
+	
+	public static int getSeatPrice(int input, String type)
+	{
+		return 0;
+	}
+	
+	public static String calculateStringTotal(int total)
+	{
+		return totalS;
+	}
+}
